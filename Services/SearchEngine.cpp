ad2191d9a1d3fb4cc1df2391e897cd28206257bd
RuseAlice <alicegeorgiana98@yahoo.com>
2021-05-25 21:56:01 +0300

    searchEngine

--- Services/SearchEngine.cpp
+++ Services/SearchEngine.cpp
@@ -2,3 +2,44 @@
 // Created by alice on 25.05.2021.
 //
 
+#include "SearchEngine.h"
+
+map<int,Haina> SearchEngine::cautare(map<int,Haina> haine, string nume) {
+    map<int,Haina> haineNume;
+    for (auto &item : haine)
+        if(item.second.getDenumire().find(nume) != std::string::npos)
+            haineNume.insert(pair<int,Haina>(item.first, item.second));
+    return haineNume;
+}
+
+map<int,Haina> SearchEngine::cautare(map<int,Haina> haine, PiesaVestimentara piesa) {
+    map<int,Haina> haineTip;
+    for (auto &item : haine)
+        if(item.second.getPiesaVestimentara() == piesa)
+            haineTip.insert(pair<int,Haina>(item.first, item.second));
+    return haineTip;
+}
+
+map<int,Haina> SearchEngine::cautare(map<int,Haina> haine, Culoare culoare) {
+    map<int,Haina> haineCuloare;
+    for (auto &item : haine)
+        if(item.second.getCuloare() == culoare)
+            haineCuloare.insert(pair<int,Haina>(item.first, item.second));
+    return haineCuloare;
+}
+
+map<int,Haina> SearchEngine::cautare(map<int,Haina> haine, Stil stil) {
+    map<int,Haina> haineStil;
+    for (auto &item : haine)
+        if(item.second.getStil() == stil)
+            haineStil.insert(pair<int,Haina>(item.first, item.second));
+    return haineStil;
+}
+
+map<int,Haina> SearchEngine::cautare(map<int,Haina> haine, Material material) {
+    map<int,Haina> haineMaterial;
+    for (auto &item : haine)
+        if(item.second.getMaterial() == material)
+            haineMaterial.insert(pair<int,Haina>(item.first, item.second));
+    return haineMaterial;
+}
--- Services/SearchEngine.h
+++ Services/SearchEngine.h
@@ -4,5 +4,16 @@
 
 #ifndef DULAPP_SEARCHENGINE_H
 #define DULAPP_SEARCHENGINE_H
+#include "../Entities/Haina.h"
+#include "HaineManager.h"
 
+class SearchEngine {
+public:
+    SearchEngine(){};
+    map<int,Haina> cautare(map<int,Haina> haine, string nume);
+    map<int,Haina> cautare(map<int,Haina> haine, PiesaVestimentara piesa);
+    map<int,Haina> cautare(map<int,Haina> haine, Culoare culoare);
+    map<int,Haina> cautare(map<int,Haina> haine, Stil stil);
+    map<int,Haina> cautare(map<int,Haina> haine, Material material);
+};
 #endif //DULAPP_SEARCHENGINE_H
