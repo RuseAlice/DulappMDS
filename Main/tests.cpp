8c8043f51177d3ad49e3b12f4de17cd7be963a46
bogdan <raiabogdan2@gmail.com>
2021-05-27 14:21:20 +0300

    Completari teste

--- Main/tests.cpp
+++ Main/tests.cpp
@@ -2,6 +2,8 @@
 #include "../Entities/Consumable.h"
 #include "../Services/DulapManager.h"
 #include "../Entities/Haina.h"
+#include "../Services/HaineManager.h"
+#include "../Services/SearchEngine.h"
 #include <thread>
 
 TEST_CASE("Testeaza Consumabilele (unit)", "[consumabile]") {
@@ -59,4 +61,64 @@ TEST_CASE("Testare Haine (unit)", "[haine]") {
         Haina h3 ("sacou", jacheta, Bleumarin, business, lana);
         REQUIRE(h3.verificarePotrivire(Bleumarin, business, 15.0) == true);
     }
+}
+
+TEST_CASE("Teste HaineManager (unit si integration)", "[haine][dulap]"){
+    Haina h1("camasa simpla", top, Alb, casual, bumbac);
+    Haina h2 ("geaca verde", jacheta, Verde, business, stofa);
+    Haina h3 ("rochie rosie", piesaUnica, Rosu, casual, matase);
+    Haina h4 ("blugi", pantaloni, Albastru, casual, denim);
+    Haina h5 ("tricou flori", top, Mov,casual, bumbac);
+    Haina h6 ("geaca fas", jacheta,Albastru, casual, fas);
+    Haina h7("helanca", top, Bej,business, poliester);
+    Haina h8 ("fusta plisata", pantaloni, Gri, business, stofa);
+    map<int, Haina> haine;
+    haine.insert(haine.begin(), pair<int, Haina>(1,h1));
+    haine.insert(pair<int, Haina>(2,h2));
+    haine.insert(pair<int, Haina>(3,h3));
+    haine.insert(pair<int, Haina>(4,h4));
+    haine.insert(pair<int, Haina>(5,h5));
+    haine.insert(pair<int, Haina>(6,h6));
+    haine.insert(pair<int, Haina>(7,h7));
+    haine.insert(pair<int, Haina>(8, h8));
+    HaineManager HM(haine, 8);
+    REQUIRE(HM.getNrUmerase() == 8);
+    //HM.vizualizareHaina(h4);
+    HM.setNrUmerase(10);
+    REQUIRE(HM.getNrUmerase() == 10);
+    HM.editareHaina(h4, "sacou", jacheta, Gri, business, bumbac); //editarea se face in dulap
+    REQUIRE(HM.getHaine().find(4)->second.getDenumire() == "sacou"); // ne uitam in dulap
+    HM.stergereHaina(h6);
+    //REQUIRE(HM.getNrUmerase() == 5);  //fals pt ca doar pentru ca haina e eliminata din dulap, asta nu inseamna ca si umerasul dispare
+    REQUIRE(HM.getHaine().count(6) == 0); //cu find(6) == haine.end() nu merge
+    HM.generareTinuta(15.0, false, casual);
+    HM.generareTinuta(30.0, false, business); // de ce nu merge??
+}
+
+TEST_CASE("Teste SearchEngine (unit si integration)", "[haine][search]") {
+    Haina h1("camasa simpla", top, Alb, casual, bumbac);
+    Haina h2 ("geaca verde", jacheta, Verde, business, stofa);
+    Haina h3 ("rochie rosie", piesaUnica, Rosu, casual, matase);
+    Haina h4 ("blugi", pantaloni, Albastru, casual, denim);
+    Haina h5 ("tricou flori", top, Mov,casual, bumbac);
+    Haina h6 ("geaca fas", jacheta,Albastru, casual, fas);
+    Haina h7 ("camasa cu volane", top, Negru, formal, poliester);
+    Haina h8 ("camasa simpla", top, Alb, formal, bumbac);
+    map<int, Haina> haine;
+    haine.insert(haine.begin(), pair<int, Haina>(1,h1));
+    haine.insert(pair<int, Haina>(2,h2));
+    haine.insert(pair<int, Haina>(3,h3));
+    haine.insert(pair<int, Haina>(4,h4));
+    haine.insert(pair<int, Haina>(5,h5));
+    haine.insert(pair<int, Haina>(6,h6));
+    haine.insert(pair<int, Haina>(7,h7));
+    haine.insert(pair<int, Haina>(8, h8));
+    SearchEngine SE;
+
+    map<int,Haina> haineCasual = SE.cautare(haine, casual);
+    REQUIRE(haineCasual.size() == 5);
+    //for (auto &item : haineCasual)
+    //    cout << item.second.afisare();
+    map<int,Haina> camasi = SE.cautare(haine, "camasa");
+    REQUIRE(camasi.size() == 3);
 }
